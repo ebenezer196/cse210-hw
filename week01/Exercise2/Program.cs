47d9b0832d13861d1c13105e06a4d52a97fd0db5
Ebenezer <ebenldsgmail.com>
2026-01-10 13:25:55 +0000

    added grade course

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask the user their grade and store the variable
+        Console.WriteLine("What is your grade percentage ? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        //Let's determine the letter grade for a course 
+
+        string letter ;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+           letter = "F"; 
+        }
+
+        //Let's determine the sign
+        int lastdigit = grade %10 ;
+        string sign = "";
+
+        if (lastdigit >=7)
+        {
+            sign = "+";
+        }
+
+        else if (lastdigit <3)
+        {
+            sign = "-";
+        }
+       
+       if (letter  == "A" && sign == "+" )
+        {
+            sign = ""; 
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        //Lets display final grade 
+        Console.WriteLine($"Your grade is : {letter} {sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine ("Congratulation you passed the course ");
+        }
+
+        else
+        {
+            Console.WriteLine ("Don't give, next time will be better !");
+        }
+
     }
 }
