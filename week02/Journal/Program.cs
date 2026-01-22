4bf1120ad9f7525363ea24e73bc24daa94b905e4
Ebenezer <ebenldsgmail.com>
2026-01-22 22:14:32 +0000

    Add journal entry

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        string choice = "";
+
+        // EXCEED REQUIREMENTS:
+        
+
+        while (choice != "5")
+        {
+            Console.WriteLine("\nJournal Menu");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+            Console.Write("Choose an option: ");
+
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Entry entry = new Entry();
+
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(entry._prompt);
+                Console.Write("> ");
+                entry._response = Console.ReadLine();
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("Enter filename to save: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Enter filename to load: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+        }
     }
 }
