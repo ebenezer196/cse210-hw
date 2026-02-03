b958f6206ad41b4ce9e5ee354c33542db0ca3245
Ebenezer <ebenldsgmail.com>
2026-02-03 19:47:38 +0000

    Add comment class

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,11 @@
+public class Comment
+{
+    public string AuthorName { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string authorName, string text)
+    {
+        AuthorName = authorName;
+        Text = text;
+    }
+}
