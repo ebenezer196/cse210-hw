95ea9e91472c809c174da7580ba01d4c8f5f0955
Ebenezer <ebenldsgmail.com>
2026-02-12 23:52:46 +0000

    Add SimpleGoal class

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,28 @@
+public class SimpleGoal : Goal
+{
+    public SimpleGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+    }
+
+    public override int RecordEvent()
+    {
+        if (!_isComplete)
+        {
+            _isComplete = true;
+            return _points;
+        }
+
+        return 0;
+    }
+
+    public override string GetStatus()
+    {
+        return $"[{(_isComplete ? "X" : " ")}] {_name}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"Simple|{_name}|{_description}|{_points}|{_isComplete}";
+    }
+}
