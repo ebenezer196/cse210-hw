e88d57947fb02f81f99fd873a7f18203eda73923
Ebenezer <ebenldsgmail.com>
2026-02-17 23:28:12 +0000

    main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Liste polymorphique
+        List<Activity> activities = new List<Activity>();
+
+        // Création des objets
+        activities.Add(new Running("03 Nov 2022", 30, 4.8));
+        activities.Add(new Cycling("03 Nov 2022", 45, 20.0));
+        activities.Add(new Swimming("03 Nov 2022", 40, 50));
+
+        // Affichage
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
